6e3c59668350059b755a6dd9911ec3ccd68920b8
nekocode <syfyw@qq.com>
2015-08-11 18:36:57 +0800

    change some source files to kotlin

---
+++ app/src/main/java/cn/nekocode/baseframework/ui/activity/MainActivity.kt
@@ -0,0 +1,101 @@
+package cn.nekocode.baseframework.ui.activity
+
+import android.os.Bundle
+import android.os.Message
+import android.support.v7.widget.RecyclerView
+import android.widget.TextView
+import android.widget.Toast
+
+import com.google.gson.Gson
+
+import java.util.Objects
+
+import cn.nekocode.baseframework.R
+import cn.nekocode.baseframework.model.Weather
+import cn.nekocode.baseframework.rest.API
+import cn.nekocode.baseframework.rest.APIFactory
+import cn.nekocode.baseframework.ui.activity.helper.BaseActivity
+import cn.nekocode.baseframework.ui.listener.Refreshable
+import rx.Observable
+import rx.Observer
+import rx.Subscriber
+import rx.android.schedulers.AndroidSchedulers
+import rx.functions.Func1
+import rx.subjects.BehaviorSubject
+
+import kotlinx.android.synthetic.activity_main.*;
+import kotlin.properties.Delegates
+
+public class MainActivity : BaseActivity<MainActivity>(), Refreshable {
+    val api: API
+        get() {
+            return APIFactory.getInstance(this)
+        }
+
+    val gson: Gson
+        get() {
+            return APIFactory.getGson()
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super<BaseActivity>.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //        Config.Setting setting = Config.loadSetting();
+        //        Config.saveSetting();
+
+        refresh()
+
+
+        //        Observable.from(new String[]{"101010100"}).flatMap(new Func1<String, Observable<Weather>>() {
+        //            @Override
+        //            public Observable<Weather> call(String s) {
+        //                return api.getWeather(s);
+        //            }
+        //        }).filter(new Func1<Weather, Boolean>() {
+        //            @Override
+        //            public Boolean call(Weather weather) {
+        //                return weather.getWeatherInfo().getCity().equals("北京");
+        //            }
+        //        }).subscribeOn(Schedulers.io())
+        //                .observeOn(AndroidSchedulers.mainThread())
+        //                .subscribe(new Observer<Weather>() {
+        //                    @Override
+        //                    public void onCompleted() {
+        //                    }
+        //
+        //                    @Override
+        //                    public void onError(Throwable e) {
+        //                    }
+        //
+        //                    @Override
+        //                    public void onNext(Weather weather) {
+        //                        textView.setText(weather.getWeatherInfo().getCity());
+        //                    }
+        //                });
+    }
+
+    override fun handler(msg: Message) {
+    }
+
+    override fun refresh(vararg objects: Any) {
+        if (objects.size() == 0) {
+            val observable = api.getWeather("101010100")
+            observable.observeOn(AndroidSchedulers.mainThread()).subscribe(object : Observer<Weather> {
+                override fun onCompleted() {
+                }
+
+                override fun onError(e: Throwable) {
+                }
+
+                override fun onNext(weather: Weather) {
+                    refresh(weather)
+                }
+            })
+
+        } else {
+            val weather = objects[0] as Weather
+            textView.setText(weather.getWeatherInfo().getCity())
+        }
+    }
+}
--- app/src/main/java/cn/nekocode/baseframework/ui/adapter/ResultAdapter.kt
+++ app/src/main/java/cn/nekocode/baseframework/ui/adapter/ResultAdapter.kt
@@ -8,20 +8,37 @@ import android.widget.ImageView;
 
 import java.util.List;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 import cn.nekocode.baseframework.R;
 import cn.nekocode.baseframework.model.Weather;
 import cn.nekocode.baseframework.ui.adapter.helper.HelperAdapter;
 import cn.nekocode.baseframework.ui.adapter.helper.HelperItemViewHolder;
+import com.squareup.picasso.Picasso
+import java.util
 
 /**
  * Created by nekocode on 2015/7/22.
  */
-public class ResultAdapter extends HelperAdapter<Weather> {
-    static final int TYPE_ITEM = 0;
+class ResultAdapter(context: Context, list: List<Weather>?) : HelperAdapter<Weather>(context, list) {
 
-    public ResultAdapter(Context context, List<Weather> list) {
+    val TYPE_ITEM = 0;
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder? {
+        when (viewType) {
+            TYPE_ITEM -> {
+                val v = layoutInflater.inflate(R.layout.item_result, parent, false)
+                return ItemViewHolder(v);
+            }
+
+        }
+
+        throw UnsupportedOperationException()
+    }
+
+    override fun getItemCount() = list?.size() ?:0
+
+
+
+    public ResultAdapter(Context context, util.List<Weather> list) {
         super(context, list);
     }
 
@@ -54,13 +71,25 @@ public class ResultAdapter extends HelperAdapter<Weather> {
         }
     }
 
-    static class ItemViewHolder extends HelperItemViewHolder {
-        @InjectView(R.id.imageView)
-        ImageView imageView;
+//    private class ItemViewHolder : HelperItemViewHolder() {
+//        ImageView imageView;
+//
+//        ItemViewHolder(View view) {
+//            super(view);
+//        }
+//    }
 
-        ItemViewHolder(View view) {
-            super(view);
-            ButterKnife.inject(this, view);
+
+    private class ItemViewHolder(view: View, var onItemClickListener: ((Weather) -> Unit)?) : HelperItemViewHolder(view) {
+
+        public val image: ImageView
+
+
+        fun setItem(weather : Weather) {
+            itemView?.setOnClickListener() { onItemClickListener?.invoke() }
+//            Picasso.with(itemView.getContext()).load(weather.getWeatherInfo().getCity()).centerCrop().fit().into(image)
         }
     }
 }
+
+
