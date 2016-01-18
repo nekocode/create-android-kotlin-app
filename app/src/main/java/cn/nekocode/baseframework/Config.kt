94a679939b3d691a5c688ef7d68cb11f6defba00
nekocode <syfyw@qq.com>
2016-01-18 18:01:28 +0800

    deleted unused configs

--- app/src/main/java/cn/nekocode/baseframework/Config.kt
+++ app/src/main/java/cn/nekocode/baseframework/Config.kt
@@ -7,14 +7,5 @@ public class Config {
     companion object {
         // app
         val APP_NAME: String = "nekoo"
-
-        val USER_AGENT: String = "TestApp"
-
-        // OkHttp
-        val RESPONSE_CACHE_FILE: String = "reponse_cache"
-        val RESPONSE_CACHE_SIZE: Int = 10 * 1024 * 1024     // 10 MB
-        val HTTP_CONNECT_TIMEOUT: Int = 10
-        val HTTP_READ_TIMEOUT: Int = 30
-        val HTTP_WRITE_TIMEOUT: Int = 10
     }
 }
